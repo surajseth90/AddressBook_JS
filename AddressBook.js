a8d624a910a840e6adba9784d78495cb5fa4ea90
surajseth90 <surajseth90@gmail.com>
2021-05-27 11:49:45 +0530

    Added and refactore code to add and edit address book

--- AddressBook.js
+++ AddressBook.js
@@ -5,6 +5,10 @@ const ZIP_REGEX = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$');
 const PHONE_NUMBER = RegExp('[0-9]{10}$');
 const EMAIL_REGEX = RegExp('^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$');
 
+
+let addressBookArray = new Array();
+var read = require("readline-sync");
+
 class AddressBook {
     firstName;
     lastName;
@@ -38,31 +42,78 @@ class AddressBook {
         }
         if (!STATE_REGEX.test(state)) {
             console.log('Please enter valid state.');
-        }else {
+        } else {
             this.state = state;
         }
         if (!ZIP_REGEX.test(zip)) {
             console.log('Please enter valid pincode.');
-        }else {
+        } else {
             this.zip = zip;
         }
         if (!PHONE_NUMBER.test(phoneNumber)) {
             console.log('Please enter valid phone number.');
-        }else {
+        } else {
             this.phoneNumber = phoneNumber;
         }
         if (!EMAIL_REGEX.test(email)) {
             console.log('Please enter valid email ID.');
-        }else {
+        } else {
             this.email = email;
         }
     }
 }
-let addressBookArrays = new Array();
-try{
-     addressBookArrays.push(new AddressBook("Suraj", "Gupta", "Badoni", "Datia", "MP", 475686, "8962921962", "suraj@gmaill.com"));
-     addressBookArrays.push(new AddressBook("Sonali","Rathore","Padhar","Betul","MP",280001,"1234560789","sonali@gmail.com"));
-     console.log(addressBookArrays);
-}catch(e){
-    console.log(e);
+let booleanValue;
+do {
+    let action = read.questionInt("What do you want to do : \n1. Show Contacts - Press 1  \n2. Add Contact - Press 2 \n2. Edit Contact - Press 3 \n ");
+    switch (action) {
+        case 1:
+            console.log(addressBookArray);
+            break;
+
+        case 2:
+            addContact();
+            break;
+
+        case 3:
+            editContact();
+            break;
+    }
+    booleanValue = read.question("Do you want to execute again , press true/false : ")
+} while (booleanValue)
+
+function editContact() {
+    let editByFirstName = read.question("Enter First Name to be edited : ");
+    for (let i = 0; i < addressBookArray.length; i++) {
+        if (addressBookArray[i].firstName == editByFirstName) {
+            console.log("Name to be edited found");
+            addContact().firstName = addressBookArray[i].firstName;
+            addContact().lastName = addressBookArray[i].lastName;
+            addContact().address = addressBookArray[i].address;
+            addContact().city = addressBookArray[i].city;
+            addContact().state = addressBookArray[i].state;
+            addContact().zip = addressBookArray[i].zip;
+            addContact().phoneNumber = addressBookArray[i].phoneNumber;
+            addContact().email = addressBookArray[i].email;
+            console.log(addressBookArray)
+        } else console.log("Please Enter a valid First Name")
+    }
+}
+
+function addContact() {
+    let firstName = read.question("Enter First Name of the person : ");
+    let lastName = read.question("Enter last Name of the person : ");
+    let address = read.question("Enter address of the person : ");
+    let city = read.question("Enter city name : ");
+    let state = read.question("Enter state name of the person : ");
+    let zip = read.question("Enter zip : ");
+    let phoneNumber = read.question("Enter phone number of the person : ");
+    let email = read.question("Enter email ID of the person : ");
+    try {
+        let person = new AddressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        addressBookArray.push(person);
+        console.log("Contact is added. ");
+        console.log(addressBookArray);
+    } catch (e) {
+        console.log(e);
+    }
 }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+var read=require("readline-sync")
